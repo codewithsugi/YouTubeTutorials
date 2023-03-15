d0f6bbec82fc5de1d0b50bd217d727eabf363074
sugithanandan <codewithsugi@gmail.com>
2023-03-15 00:27:08 -0700

    comments in java

---
+++ Comments_Project/src/com/youtube/learning/CommentsInJava.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2023.
+ * This Project mainly for Comments
+ * 
+ */
+package com.youtube.learning;
+
+/**
+ * This class is to demonstrate about comments in Java
+ * 
+ * @author sugithanandan
+ * @since 2023
+ * @version 1.0
+ */
+public class CommentsInJava {
+
+	/*
+	 * 
+	 */
+	 private int num = 10; 
+
+	private int num1 = 10;
+	private int num2 = 10;
+	private int num3 = 10;
+	private int num4 = 10;
+	private int num5 = 10;   // to format Ctrl + Shift + F
+	
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub -- Single line Comment
+		
+ 
+	}
+
+}
---
+++ Comments_Project/src/com/youtube/learning/WOComments.java
@@ -0,0 +1,10 @@
+package com.youtube.learning;
+
+public class WOComments {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
