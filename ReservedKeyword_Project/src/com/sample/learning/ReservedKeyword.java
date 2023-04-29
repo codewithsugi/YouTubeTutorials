9bcb1eb18377b0cb5af651b18c0367d1366b55ab
sugithanandan <codewithsugi@gmail.com>
2023-04-29 00:28:52 -0700

    reserved keywords

---
+++ ReservedKeyword_Project/src/com/sample/learning/ReservedKeyword.java
@@ -0,0 +1,63 @@
+package com.sample.learning;
+
+/*
+ * // Basic (7)
+ * 
+ * class, static, void, return, package, import, new
+ * 
+ * //data types (8)
+ * 
+ * byte, short, int, long, float, double, char, boolean
+ * 
+ * //looping statements (9)
+ * 
+ * if, else, for, while, do, switch, case, break,
+ * continue
+ * 
+ * //access modifiers : (4) 
+ * 
+ * default, private, public, protected
+ * 
+ * //inheritance and Abstraction (6) 
+ * 
+ * interface, implements, extends, super,
+ * this, abstract
+ * 
+ * //Exception Handling: (5)
+ * 	
+ * try, catch, finally, throw, throws
+ * 
+ * //constants (2)
+ * 
+ *  const (not used), final
+ * 
+ * //few other (9) --> 50 
+ * 
+ * instanceof, enum, assert, transient, volatile, native,
+ * synchronized, strictfp, goto (not used)
+ */
+
+
+public class ReservedKeyword{	
+	
+	public static void main(String[] args) {
+		
+		ReservedKeyword object = new ReservedKeyword();
+				
+	}
+	
+	
+}
+
+
+
+
+
+
+		   
+		   
+		   
+		   
+		   
+		   
+		   
